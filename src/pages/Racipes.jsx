859a221b91bf36bffed47b2bb16c7b2452c27245
sethyadav <sethyadav169@gmail.com>
2026-09-04 12:40:03 +0530

    Refactor Racipes component to utilize recipeContext for dynamic recipe rendering

--- src/pages/Racipes.jsx
+++ src/pages/Racipes.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
-
+import { useContext } from 'react'
+import  {recipeContext} from '../context/RacipeContext'
 const Racipes = () => {
-  return (
-    <div>
-      recipes
+
+  const {data} = useContext(recipeContext);
+
+  const renderRacipes = data.map((racipe) => (
+    <div key={racipe.id}>
+      <h3>{racipe.name}</h3>
+      <p>{racipe.description}</p>
     </div>
-  )
-}
+  ));
+
+  return <div>{renderRacipes}</div>
+};
 
 export default Racipes
